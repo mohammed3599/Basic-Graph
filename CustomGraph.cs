edeef85f2ec4d2f689bfd2893a86e71b8ca96a89
mohammed3599 <kataloni1999@gmail.com>
2023-07-18 12:49:39 +0400

    Implementing Basic Graph

---
+++ CustomGraph.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Graph
+{
+    internal class CustomGraph
+    {
+        private Dictionary<String, List<String>> graph;
+        public CustomGraph() 
+        {
+            graph = new Dictionary<String, List<String>>();
+        }
+        public void AddVertex(String vertex) 
+        {
+            if(!graph.ContainsKey(vertex))
+            {
+                graph[vertex] = new List<String>();
+            }
+        }
+        public void AddEdge(String vertex1, String vertex2) 
+        {
+            if (graph.ContainsKey(vertex1) && graph.ContainsKey(vertex2)) 
+            {
+                graph[vertex1].Add(vertex2);
+                graph[vertex2].Add(vertex1);
+            }
+        }
+        public List<String> GetNeighbor(String vertex) 
+        {
+            if (graph.ContainsKey(vertex))
+            {
+                return graph[vertex];
+            }
+
+            return new List<string> ();
+        }
+        public List<String> GetVertices()
+        {
+            List<String> vertices = new List<String>(graph.Keys);
+            return vertices;
+        }
+        public Dictionary<String, List<String>> PrintGraph() 
+        {
+            return graph;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Basic_Graph
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomGraph graph = new CustomGraph();
+
+            //Adding Vertices
+            graph.AddVertex("A");
+            graph.AddVertex("B");
+            graph.AddVertex("C");
+            graph.AddVertex("D");
+
+            //Adding Edges
+            graph.AddEdge("A", "B");
+            graph.AddEdge("B", "C");
+            graph.AddEdge("C", "D");
+            graph.AddEdge("D", "A");
+
+            List<String> neighbors = graph.GetNeighbor("A");
+            Console.WriteLine("Neighbor of A: " + string.Join(" ," , neighbors));
+
+            //Printing the Graph
+            Dictionary<String, List<String>> graphStructure = graph.PrintGraph();
+            foreach(var vertex in graphStructure) 
+            {
+                Console.WriteLine("Vertex: " + vertex.Key);
+                Console.WriteLine("Neighbors: " + string.Join(", " , vertex.Value));
+            }
+        }
+    }
+}
